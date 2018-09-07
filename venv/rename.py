d9691f0c64004d647ba27b7048325fe2ae453786
ChangHyun <heypaprika@naver.com>
2018-09-07 19:07:52 +0900

    new project

---
+++ venv/rename.py
@@ -0,0 +1,24 @@
+import os
+import cv2
+
+
+imsize = 600
+sidx = 2000
+
+imglist = os.listdir('./')
+
+for impath in imglist:
+    if '.JPG' in impath:
+        img = cv2.imread(impath)
+        im = cv2.resize(img, (imsize, imsize))
+        savename = '{}.png'.format(int(sidx))
+        cv2.imwrite(savename, im)
+        sidx += 1
+    else:
+        pass
+
+cv2.waitKey(-1)
+
+
+
+#print(imglist)
