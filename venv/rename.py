6c028ba93148f9d9cddd119d6f828220e41b3891
ChangHyun <heypaprika@naver.com>
2018-09-07 19:52:31 +0900

    coimmit

--- venv/rename.py
+++ venv/rename.py
@@ -10,7 +10,7 @@ imglist = os.listdir('./')
 for impath in imglist:
     if '.JPG' in impath:
         img = cv2.imread(impath)
-        im = cv2.resize(ifjaoisdfnoaiwjefomg, (imsize, imsize))
+        im = cv2.resize(ifjaoisdfnoaiwjefotoiwrjeoiqwejroiqjreioqjeroiqj23rmg, (imsize, imsize))
         savename = '{}.png'.format(int(sidx))
         cv2.imwrite(savename, im)
 
